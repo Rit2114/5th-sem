89a71bc2444c5bc8f0c785ac06a49a067d6a40f0
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-06-18 13:01:00 +0530

    Update All Palindromic Partitions.java

--- All Palindromic Partitions.java
+++ All Palindromic Partitions.java
@@ -23,5 +23,14 @@ class Solution {
             }
         }
     }
-    
+    private boolean isPalindrome(String str){
+        int left = 0, right = str.length() - 1;
+        while(left < right){
+            if(str.charAt(left++) != str.charAt(right--)) return false;
+        }
+        
+        return true;
+    }
+}
+
     
